6e6827d8b777cdb81e10cd05372b0ed4efc1e593
Zag207 <62791287+Zag207@users.noreply.github.com>
2023-07-31 23:59:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,30 +5,32 @@ let select = document.querySelector("select");
 let form = document.querySelector("form");
 let header = document.querySelector("header");
 
-let btn = document.querySelector("button");
-
-function func(clas) {
+let func = (clas, color_class, e) => {
   body.className = clas;
-  header.classList.toggle("new_color");
-}
+  header.className = color_class;
+  e.preventDefault();
+  form.reset();
+};
 
-form.addEventListener("submit", (event) => {});
-btn.addEventListener("click", () => {
-  if (checkbox1.checked) {
-    func("hehehe");
-    return;
-  }
-
-  if (checkbox2.checked) {
-    func("Ouuuuu");
-    return;
-  }
-
+form.addEventListener("submit", (e) => {
   if (select.value == "5") {
     let a = confirm("А может советские?)");
     select.value = "";
     if (a) {
       window.location.href = "https://vk.com/video482407910_456239029";
     }
+    e.preventDefault();
+    form.reset();
+    header.className = "";
+    return;
+  }
+  if (checkbox1.checked) {
+    func("hehehe", "base_color", e);
+    return;
+  }
+
+  if (checkbox2.checked) {
+    func("Ouuuuu", "new_color_white", e);
+    return;
   }
 });
